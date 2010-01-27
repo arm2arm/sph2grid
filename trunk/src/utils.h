7bbe60b0afb116bd34f4ec5e8c7620c0a2c3b394
arm2arm <arm2arm@0b241152-ebc4-11de-a70e-4b51a9783938>
2010-01-27 20:29:21 +0000

    CImg library based rendering added

--- trunk/src/utils.h
+++ trunk/src/utils.h
@@ -2,6 +2,8 @@
 #define _CRange_
 #include <iostream>
 #include <algorithm>
+#include <cassert>
+#include <vector>
 using std::min;
 using std::max;
 using std::cout;
@@ -40,6 +42,79 @@ float mydrand48(void);
 #ifndef M_PI
 #define M_PI 3.1415926535897932384626433832795f
 #endif
+
+
+
+
+
+
+
+
+template<class T>
+class CKernel{
+public:
+        CKernel(const int dim=3):m_dim(dim){
+                if((m_dim<1)||(m_dim>20))
+                        {
+                        std::cout<<"Specify the Normalization constant for dimensions > 20"<<endl;
+                        
+                        }
+                AllocateTable();
+                };
+        ~CKernel(){};
+        virtual T W(T u)=0;
+protected:
+        void AllocateTable()
+                {
+                KernelRad.resize(KERNEL_TABLE+2);//+2 allow to go in the loops  KERNEL_TABLE+1;
+                Kernel.resize(KERNEL_TABLE+2);
+                };
+        virtual void    InitTable()=0;
+protected:
+        static const int KERNEL_TABLE=1000;
+        std::vector<T> KernelRad,Kernel;
+        int m_dim;
+        std::string TypeOfKernel;
+        };
+
+
+
+template<class T>
+class CEpanechikov : public CKernel<T>
+        {
+        public:
+                CEpanechikov(int dim):CKernel<T>(dim){this->TypeOfKernel=std::string("Epanechikov kernel= Cd*(1-u)");InitTable();};
+        private:
+        virtual void    InitTable(){
+                static double
+                        fep[]={0.75000113,0.63661975,0.59683102,0.60792705,0.66492015,0.77403670,0.95242788,1.2319173,1.6674189,2.3527875,3.4499109,5.2424031,8.2360444,13.349647,22.283674,38.243824,67.384374,121.73344,225.21478,426.23651};
+                T f1=(T)fep[this->m_dim-1];//Epanechikov
+                cout<<"Normalization constant of Kernel type "<< this->TypeOfKernel<<": "<<f1<<endl;
+                int i=0;
+                for(i=0;i<=this->KERNEL_TABLE+1;i++)
+                        {
+                        this->KernelRad[i] = ((T)i)/(T)this->KERNEL_TABLE;
+                        this->Kernel[i] = f1 *(1-this->KernelRad[i]*this->KernelRad[i]);
+                        }
+                this->Kernel[this->KERNEL_TABLE+1] =(T)0.0;
+                };
+        public:
+        virtual T W(T u){
+                int k;
+                T wk=0.0;
+                if(u<(T)1.0)
+                        {
+                        k = (int)(u*this->KERNEL_TABLE);
+                        wk =( this->Kernel[k]  + (this->Kernel[k+1]-this->Kernel[k])*(u-this->KernelRad[k])*this->KERNEL_TABLE);
+                        //rhoxyz+=wk*Part[pqStartA[i].p].Mass;
+                        }
+                return wk;
+                };
+
+        };
+
+
+
 
 #endif
 
